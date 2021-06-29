70e8d65127a2c0dbc54e9e02371a13e9b1d9dee0
Nermin Bosnjak <nermin@MacBook-Pro.local>
2021-06-29 09:33:35 +0200

    Added intersection observer

--- components/CounterItem/CounterItem.js
+++ components/CounterItem/CounterItem.js
@@ -1,19 +1,37 @@
 import React, { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
 import { animate } from "framer-motion";
 
 export default function CounterItem({ from, to, aditional = "" }) {
   const nodeRef = useRef();
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
   useEffect(() => {
     const node = nodeRef.current;
-
-    const controls = animate(from, to, {
-      duration: 1,
-      onUpdate(value) {
-        node.textContent = value.toFixed() + aditional;
-      },
-    });
-
+    const controls =
+      inView &&
+      animate(from, to, {
+        duration: 1,
+        onUpdate(value) {
+          node.textContent = value.toFixed() + aditional;
+        },
+      });
     return () => controls.stop();
   }, [from, to]);
-  return <span ref={nodeRef} />;
+
+  return (
+    <div ref={ref}>
+      <span ref={nodeRef} />
+    </div>
+  );
 }
+
+// const Component = () => {
+//   return (
+//     <div ref={ref}>
+//       <h2>{`Header inside viewport ${inView}.`}</h2>
+//     </div>
+//   );
+// };
