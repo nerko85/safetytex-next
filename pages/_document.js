abace0ad8e68be1a7a8f327208b975119c804654
nerko85 <31403849+nerko85@users.noreply.github.com>
2022-12-21 17:37:13 +0100

    Added new tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,19 @@ export default class CustomDocument extends Document {
     return (
       <Html className="scroll-smooth antialiased">
         <Head>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
+        `}
+          </Script>
           <Script id="google-tag-manager" strategy="afterInteractive">
             {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
